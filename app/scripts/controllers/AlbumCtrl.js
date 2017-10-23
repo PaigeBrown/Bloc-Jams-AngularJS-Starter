3a89b95d1189449ce2eb6ddaaa1b6a4eb6ddb65f
Paige Brown <brown.emily.paige@gmail.com>
2017-10-23 17:11:43 -0400

    assignment 5

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,26 @@
+// var albumData = albumPicasso
+//
+// var albumData= function ($scope) {
+//   $scope.title = The Colors;
+//   $scope.artist = Pablo Picasso;
+//   $scope.release-info = 1909 Spanish Records;
+// }
+//
+//
+// <div class="album-view-details column half">
+//                   <h2 class="album-view-title">{{ album.albumData.title}}</h2>
+//                   <h3 class="album-view-artist">Pablo Picasso</h3>
+//                   <h5 class="album-view-release-info">1909 Spanish Records</h5>
+//               </div>
+
+
+(function() {
+     function AlbumCtrl() {
+       this.albumData = angular.copy(albumPicasso)
+       console.log(angular.copy(albumPicasso))
+     }
+
+     angular
+         .module('blocJams')
+         .controller('AlbumCtrl', AlbumCtrl);
+ })();
