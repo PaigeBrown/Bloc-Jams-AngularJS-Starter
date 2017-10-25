08c315d13f2944fa9995815bbc9df8cbcce65eed
Paige Brown <brown.emily.paige@gmail.com>
2017-10-25 12:13:39 -0400

    assignment 7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,7 +3,7 @@
          var SongPlayer = {};
 
          var currentSong = null;
-         
+
          /**
         * @desc Buzz object audio file
         * @type {Object}
@@ -31,11 +31,21 @@
             currentSong = song;
          };
 
+         /**
+        * @function playSong
+        * @desc Plays the currentBuzzObject and sets the property of the song Object to true
+        * @param {Object} song
+        */
+
+         var playSong = function(song){
+           currentBuzzObject.play();
+           song.playing = true
+         }
+
          SongPlayer.play = function(song) {
            if (currentSong !== song) {
              setSong(song);
-             currentBuzzObject.play();
-             song.playing = true;
+             playSong(song);
            } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
