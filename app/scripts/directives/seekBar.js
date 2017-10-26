eaaa15c84714d6a1ff6e6514dfa1f9cce0ce22b0
Paige Brown <brown.emily.paige@gmail.com>
2017-10-26 12:19:36 -0400

    checkpoint10

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -14,12 +14,23 @@
            templateUrl: '/templates/directives/seek_bar.html',
            replace: true,
            restrict: 'E',
-           scope: {},
+           scope: {
+                onChange: '&'
+           },
            link: function(scope, element, attributes) {
              scope.value = 0;
              scope.max = 100;
 
              var seekBar = $(element);
+
+             attributes.$observe('value', function(newValue) {
+                  scope.value = newValue;
+              });
+
+              attributes.$observe('max', function(newValue) {
+                  scope.max = newValue;
+              });
+
 
              var percentString = function () {
                  var value = scope.value;
@@ -27,6 +38,14 @@
                  var percent = value / max * 100;
                  return percent + "%";
              };
+
+             var notifyOnChange = function(newValue) {
+                if (typeof scope.onChange === 'function') {
+                    scope.onChange({
+                      value: newValue
+                    });
+                }
+            };
 
              scope.fillStyle = function() {
                  return {
@@ -44,6 +63,7 @@
              scope.onClickSeekBar = function(event) {
                  var percent = calculatePercent(seekBar, event);
                  scope.value = percent * scope.max;
+                 notifyOnChange(scope.value);
              };
 
              scope.trackThumb = function() {
@@ -51,6 +71,7 @@
                      var percent = calculatePercent(seekBar, event);
                      scope.$apply(function() {
                          scope.value = percent * scope.max;
+                         notifyOnChange(scope.value);
                      });
                  });
 
